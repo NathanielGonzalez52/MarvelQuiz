9d7ddb4a136af28a3c9041625311ea0d17a777d8
Nathan Gonzalez <nathanielgonzalez52@gmail.com--global user.email nathanielgonzalez52@gmail.com>
2022-08-21 21:20:49 -0700

    Deselect button on next

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-// var x = 0;
-// while (x < 1000) {
-// 	var randNum = Math.random();
-// 	console.log(Math.floor(randNum * (11 + 1)));
-// 	x++;
-// }
-//
-//
-// // console.log(randNum());
-// // Questions will be asked
-
-// question template
-
-// id: 11,
-// q: "?",
-// a: [{ text: "", isCorrect: false },
-// 	{ text: "", isCorrect: false },
-// 	{ text: "", isCorrect: false },
-// 	{ text: "", isCorrect: false }
-// ]
 const Questions = [{
 		id: 0,
 		q: 'Which character said, "I never said you were a superhero"?',
@@ -184,32 +164,10 @@ const Questions = [{
 	}
 ]
 
-// console.log(Questions.length)
-//
 // Set start
 var start = true;
-//
-
-
-
 // Iterate
 function iterate(id) {
-	// gameStarted = true;
-	// if (gameStarted) {
-	//
-	// }
-	// var score = 0;
-	// Getting the result display section
-	// var result = document.getElementsByClassName("result");
-	// result[0].innerText = "";
-
-	// Getting the question
-
-	// const question = document.getElementById("question");
-	// Setting question title TEXT
-
-
-	// title.innerText = "Question " + Number(question);
 	// Setting the question text
 	question.innerText = Questions[id].q;
 
@@ -269,30 +227,6 @@ function iterate(id) {
 		op4.style.backgroundColor = "green";
 		selected = op4.value;
 	})
-	// // Grabbing the next button
-	// const next = document.getElementsByClassName("next");
-	//
-	// // Evaluate method
-	// next[0].addEventListener("click", () => {
-	// 	question++;
-	// 	if (selected == "true") {
-	// 		score += 1;
-	// 	} else {
-	// 		console.log("wrong");
-	// 	}
-	// })
-
-	// PREVIOUS FUNCTION
-	// const previous = document.getElementsByClassName("previous")
-	//
-	// // Previous functionality
-	// previous[0].addEventListener("click", () => {
-	//
-	// 	title.innerText = "Question " + Number(id-1);
-	// 	// Setting the question text
-	// 	question.innerText = Questions[id-1].q;
-	// })
-
 
 	// // Grabbing the evaluate button
 	// const evaluate = document.getElementsByClassName("evaluate");
@@ -317,16 +251,6 @@ function randNum() {
 	// Math.floor(randNum * (11 + 1))
 	var randQues = Math.floor(randValue * (Questions.length))
 	return randQues;
-	// if (nums.includes(randQues)) {
-	// 	console.log(randQues);
-	// 	console.log("Already used")
-	// 	nums.pop();
-	// 	console.log(nums);
-	// 	randNum()
-	// } else {
-	// 	nums.push(randQues)
-	// 	return randQues;
-	// }
 
 }
 
@@ -351,8 +275,17 @@ var id = 1;
 var pClicked = false;
 var prevClicked = 0;
 next.addEventListener("click", () => {
-
 	start = false;
+	const op1 = document.getElementById('op1');
+	const op2 = document.getElementById('op2');
+	const op3 = document.getElementById('op3');
+	const op4 = document.getElementById('op4');
+
+	op1.style.backgroundColor = "#ED1D24";
+	op2.style.backgroundColor = "#ED1D24";
+	op3.style.backgroundColor = "#ED1D24";
+	op4.style.backgroundColor = "#ED1D24";
+
 	if (prevClicked > 0) {
 		// title.innerText = "Question " + Number(nums.indexOf(id));
 		// iterate(nextQuestion.toString());
@@ -363,33 +296,19 @@ next.addEventListener("click", () => {
 	else if (id <= 9) {
 		id++;
 		var nextQuestion = randNum();
-		// console.log(nextQuestion)
-		// while (id < 11) {
-		// 	var a = randNum();
-		// 	console.log(a);
-		// 	id++;
-		// }
+
 		while (nums.includes(nextQuestion)) {
-			// console.log(nextQuestion);
 				nextQuestion = randNum();
-				// console.log("Yes, it's already been selected.");
-				// console.log(nextQuestion);
 		}
 
 		nums.push(nextQuestion);
-		// console.log(nums);
-		// console.log(nums)
+
 		title.innerText = "Question " + Number(id);
-			// nums.push
+
 		iterate(nextQuestion.toString());
 		console.log(id);
-
-// var currQues = title.innerText = "Question " + Number(nums[-1])
-		// console.log(nums[nums.length-back]);
-		// title.innerText = "Question " + Number(id);
-		// iterate(nextQuestion.toString());
-		// console.log(randNum());
 	}
+
 
 })
 
